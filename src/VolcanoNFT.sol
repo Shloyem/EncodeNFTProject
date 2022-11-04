8029a545bac467cf4a489d9191b7dc2c88293154
shloyem <39220943+Shloyem@users.noreply.github.com>
2022-11-04 11:50:03 -0400

    allow mint NFT with ether or Volcano Coin

--- src/VolcanoNFT.sol
+++ src/VolcanoNFT.sol
@@ -2,18 +2,49 @@
 pragma solidity ^0.8.13;
 
 import "openzeppelin-contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-contracts/access/Ownable.sol";
 import "openzeppelin-contracts/utils/Counters.sol";
 
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; // if using remix / npm
+// import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
+
 contract VolcanoNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
+    address public coinAddress;
+    Counters.Counter private tokenIdCounter;
 
-    constructor() ERC721("AwesomeVolcanoNft", "AVN") {}
+    constructor(address _coinAddress) ERC721("AwesomeVolcanoNft", "AVN") {
+        coinAddress = _coinAddress;
+    }
+
+    function setCoinAddress(address _coinAddress) public onlyOwner {
+        coinAddress = _coinAddress;
+    }
 
     function mint(address _to) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(_to, tokenId);
+        _mint(_to);
+    }
+
+    function _mint(address _to) private {
+        tokenIdCounter.increment();
+        _safeMint(_to, tokenIdCounter.current());
+    }
+
+    function payToMint(address _to) public payable {
+        bool executed;
+
+        if (msg.value >= 0.01 ether) {
+            payable(msg.sender).transfer(msg.value - 0.01 ether); // maybe improve
+            executed = true;
+        } else {
+            IERC20(coinAddress).transferFrom(msg.sender, address(this), 100);
+            executed = true;
+        }
+
+        require(executed, "Did not receive 0.01 ether or 100 tokens");
+
+        _mint(_to);
     }
 }
--- test/VolcanoNFT.t.sol
+++ test/VolcanoNFT.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/VolcanoNFT.sol";
+import "../src/VolcanoCoin.sol";
 import "../src/ERC721Receiver.sol";
 
 contract VolcanoNFTTest is Test {
@@ -10,11 +11,14 @@ contract VolcanoNFTTest is Test {
     address public user1;
     address public user2;
     VolcanoNFT private volcanoNFT;
+    VolcanoCoin private volcanoCoin;
     ERC721Receiver private erc721Receiver1;
     ERC721Receiver private erc721Receiver2;
+    uint nftId = 1;
 
     function setUp() public {
-        volcanoNFT = new VolcanoNFT();
+        volcanoCoin = new VolcanoCoin();
+        volcanoNFT = new VolcanoNFT(address(volcanoCoin));
         erc721Receiver1 = new ERC721Receiver();
         erc721Receiver2 = new ERC721Receiver();
         contractOwner = address(this);
@@ -29,7 +33,7 @@ contract VolcanoNFTTest is Test {
 
     function testMint() public {
         volcanoNFT.mint(user1);
-        assertEq(volcanoNFT.ownerOf(0), user1);
+        assertEq(volcanoNFT.ownerOf(nftId), user1);
     }
 
     function testNotOwnerCantMint() public {
@@ -40,32 +44,32 @@ contract VolcanoNFTTest is Test {
 
     function testHolderTransfer() public {
         volcanoNFT.mint(user1);
-        assertEq(volcanoNFT.ownerOf(0), user1);
+        assertEq(volcanoNFT.ownerOf(nftId), user1);
         vm.prank(user1); // changed to user1 from owner
-        volcanoNFT.transferFrom(user1, user2, 0);
-        assertEq(volcanoNFT.ownerOf(0), user2);
+        volcanoNFT.transferFrom(user1, user2, nftId);
+        assertEq(volcanoNFT.ownerOf(nftId), user2);
     }
 
-    function testNotHolderCantTransfer1() public {
+    function testNotHolderCantTransfer() public {
         volcanoNFT.mint(user1);
-        assertEq(volcanoNFT.ownerOf(0), user1);
+        assertEq(volcanoNFT.ownerOf(nftId), user1);
         vm.expectRevert("ERC721: caller is not token owner or approved");
         // attemp transfer from contract-owner but not NFT-owner or approved
-        volcanoNFT.transferFrom(user1, user2, 0);
+        volcanoNFT.transferFrom(user1, user2, nftId);
 
         vm.expectRevert("ERC721: caller is not token owner or approved");
         // attemp transfer from another user that is not the holder
         vm.prank(user2);
-        volcanoNFT.transferFrom(user1, user2, 0);
+        volcanoNFT.transferFrom(user1, user2, nftId);
     }
 
     function testApproveTransfer() public {
         volcanoNFT.mint(user1);
-        assertEq(volcanoNFT.ownerOf(0), user1);
+        assertEq(volcanoNFT.ownerOf(nftId), user1);
         vm.prank(user1);
-        volcanoNFT.approve(contractOwner, 0);
+        volcanoNFT.approve(contractOwner, nftId);
         vm.prank(contractOwner);
-        volcanoNFT.transferFrom(user1, user2, 0);
-        assertEq(volcanoNFT.ownerOf(0), user2);
+        volcanoNFT.transferFrom(user1, user2, nftId);
+        assertEq(volcanoNFT.ownerOf(nftId), user2);
     }
 }
